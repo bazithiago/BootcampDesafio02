732d97ef0b38480b90de8be4c15d8170f8658f07
Thiago <contato.vieirathiago@gmail.com>
2020-08-19 18:15:56 -0300

    Insert GET route

--- src/app.js
+++ src/app.js
@@ -11,7 +11,13 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const {title} = request.query;   
+
+  const results = title  
+      ? repositories.filter(project => project.title.includes(title))  // se tiver um title, results será igual o que tiver buscado
+      : repositories;        // se não tem, retorna todos os projects
+
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
